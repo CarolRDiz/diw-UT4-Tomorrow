3dc92bf3ce1c6ac19d72403983f97077328c7557
CarolRDiz <91955244+CarolRDiz@users.noreply.github.com>
2022-12-12 00:18:31 +0100

    Add files via upload

---
+++ dist/index.41b17edc.js
@@ -0,0 +1,9 @@
+const toggleMenuElement = document.getElementById("toggle-menu");
+const mainMenuElement = document.getElementById("main-menu");
+toggleMenuElement.addEventListener("click", ()=>{
+    console.log("funciona");
+    if (mainMenuElement.style.display === "none") mainMenuElement.style.display = "block";
+    else mainMenuElement.style.display = "none";
+});
+
+//# sourceMappingURL=index.41b17edc.js.map
